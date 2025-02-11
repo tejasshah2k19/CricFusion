9d91f47e0827ac3405c8a5a66440925221ecdd6e
Tejas Shah <tejasshah2k19@gmail.com>
2025-02-11 18:04:49 +0530

    init

---
+++ src/main/java/com/grownited/CircFusionApplication.java
@@ -0,0 +1,13 @@
+package com.grownited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircFusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grownited/CircFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grownited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
